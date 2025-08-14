16cb4a538f2e7528e356aa8f0a55c4db5c6dd922
Jhakejhosh <jhakejhosh83@gmail.com>
2025-08-14 22:53:56 +0100

    added map to contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,12 @@
 import Banner from '@/components/Banner'
+import Map from '@/components/Map'
 import React from 'react'
 
 const contact = () => {
   return (
     <div>
       <Banner heading='Contact Us' subHeading='You can reach out to us here'/>
+      <Map/>
     </div>
   )
 }
---
+++ components/Map.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import React from 'react'
+import { MapContainer } from 'react-leaflet/MapContainer'
+import { TileLayer } from 'react-leaflet/TileLayer'
+import { Marker } from 'react-leaflet/Marker'
+import { Popup } from 'react-leaflet/Popup'
+import 'leaflet/dist/leaflet.css'
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
+import 'leaflet-defaulticon-compatibility'
+import { FaMapMarkerAlt } from 'react-icons/fa'
+
+const Map = () => {
+  return (
+    <>
+        <MapContainer center={[6.514193, 3.308678]} zoom={13} scrollWheelZoom={false} style={{
+            height: '400px',
+            width: '100%'
+        }}>
+          <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[6.514193, 3.308678]}>
+    <Popup>
+      <FaMapMarkerAlt/>
+    </Popup>
+  </Marker>
+</MapContainer>
+    </>
+  )
+}
+
+export default Map
